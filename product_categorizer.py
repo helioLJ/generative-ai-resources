f61f79a359b418cebbd21501fca979cc60a32090
helioLJ <helio.lucio.jr54@gmail.com>
2023-12-14 15:22:26 -0300

    prompt template

--- product_categorizer.py
+++ product_categorizer.py
@@ -1,26 +1,42 @@
 from openai import OpenAI
 client = OpenAI()
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {
-      "role": "system",
-      "content": "Voce é um categarizador de produtos."
-    },
-    {
-      "role": "user",
-      "content": "escova de dentes"
-    }
-  ],
-  temperature=1,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  n=5
-)
+def categorizeProduct(product_name, valid_categories):
+    system_prompt = f"""
+    Voce é um categorizador de produtos.
+    Voce deve escolher uma categoria da lista abaixo:
 
-for i in range(5):
-    print(response.choices[i].message.content)
-    print("\n------------------------\n")
+    #### Lista de categorias válidas
+    {valid_categories}
+    #### Exemplo
+    bola de tenis
+    Esportes
+    """
+
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "system",
+        "content": system_prompt
+        },
+        {
+        "role": "user",
+        "content": product_name
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    #   n=5
+    )
+    print(response.choices[0].message.content)
+
+
+
+while True:
+    print("Digite o nome do produto! ")
+    product_name = input()
+    categorizeProduct(product_name, "Beleza, Esportes, Entretenimento, Outros")
